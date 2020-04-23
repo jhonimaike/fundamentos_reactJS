f94159281dd67e80eb55853f37f7128027caa79b
Jhoni Diedrich <jhoni.diedrich@pti.org.py>
2020-04-23 15:06:34 -0400

    challenge done

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
 
 import filesize from 'filesize';
 
@@ -11,6 +11,7 @@ import { Container, Title, ImportFileContainer, Footer } from './styles';
 
 import alert from '../../assets/alert.svg';
 import api from '../../services/api';
+import history from '../../services/history';
 
 interface FileProps {
   file: File;
@@ -20,22 +21,33 @@ interface FileProps {
 
 const Import: React.FC = () => {
   const [uploadedFiles, setUploadedFiles] = useState<FileProps[]>([]);
-  const history = useHistory();
+  // history = useHistory();
 
-  async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+  async function submitFile(): Promise<void> {
+    const data = new FormData();
 
-    // TODO
+    if (!uploadedFiles.length) return;
+
+    data.append('file', uploadedFiles[0].file, uploadedFiles[0].name);
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
-  function submitFile(files: File[]): void {
-    // TODO
+  function handleUpload(files: File[]): void {
+    const uploadFiles = files.map((file: File) => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size),
+    }));
+
+    console.log(uploadFiles);
+
+    setUploadedFiles(uploadFiles);
   }
 
   return (
@@ -44,7 +56,7 @@ const Import: React.FC = () => {
       <Container>
         <Title>Importar uma transação</Title>
         <ImportFileContainer>
-          <Upload onUpload={submitFile} />
+          <Upload onUpload={handleUpload} />
           {!!uploadedFiles.length && <FileList files={uploadedFiles} />}
 
           <Footer>
@@ -52,7 +64,7 @@ const Import: React.FC = () => {
               <img src={alert} alt="Alert" />
               Permitido apenas arquivos CSV
             </p>
-            <button onClick={handleUpload} type="button">
+            <button onClick={submitFile} type="button">
               Enviar
             </button>
           </Footer>
